f84b31d8c2dafe432e0e48713c054f58d76cacec
Shemaryahu Zalmanov <shemaryahuz@gmail.com>
2025-10-19 10:35:37 +0300

    added heading and app icon

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-
+      <img 
+        src='/make-money.svg' 
+        alt='make money icon' 
+        width={200}
+        />
+      <h1>Expense Manager</h1>
     </div>
   );
 }
