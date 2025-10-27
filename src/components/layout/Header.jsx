056dafc8e7cc9fea9784377ccaaa6cf59b6bc7f4
Shemaryahu Zalmanov <shemaryahuz@gmail.com>
2025-10-27 17:26:59 +0200

    added basic home page

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,14 +1,16 @@
-import { AppBar, Toolbar } from "@mui/material";
+import { AppBar, Link, Toolbar } from "@mui/material";
 
 export default function Header() {
   return (
     <AppBar component="header" position="static">
       <Toolbar>
-        <img
-          src="/money-management.svg"
-          alt="money management icon"
-          width={50}
-        />
+        <Link href="/">
+          <img
+            src="/money-management.svg"
+            alt="money management icon"
+            width={50}
+          />
+        </Link>
         <h1>Expense Manager</h1>
       </Toolbar>
     </AppBar>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,36 @@
+import { Box, Button, ButtonGroup, Container, Grid } from "@mui/material";
 
 export default function HomePage() {
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
-  )
+    <>
+      <Container>
+        <h1>Welcome to Expense Manager!</h1>
+        <p>Manage your expenses and income easily.</p>
+      </Container>
+      <Grid container spacing={2} padding={2}>
+        <Box>
+          <h2>Last Expenses:</h2>
+          <ul>
+            <li>Groceries - $50</li>
+            <li>Transport - $20</li>
+            <li>Utilities - $100</li>
+          </ul>
+        </Box>
+        <Box>
+          <h2>Current Budget:</h2>
+          <p>Expenses: $500</p>
+          <p>Income: $2000</p>
+          <p>Remaining: $1500</p>
+        </Box>
+      </Grid>
+      <ButtonGroup variant="outlined" aria-label="outlined button group">
+        <Button variant="contained" color="primary" href="/dashboard">
+          Go to Dashboard
+        </Button>
+        <Button variant="contained" color="primary" href="/budgets">
+          Go to Budgets
+        </Button>
+      </ButtonGroup>
+    </>
+  );
 }
