cd87debd4c1ca13bb21a5d63f23a8e9f53dd3c71
Shemaryahu Zalmanov <shemaryahuz@gmail.com>
2025-11-16 11:10:18 +0200

    updated routers

--- backend/src/routes/categoriesRouter.js
+++ backend/src/routes/categoriesRouter.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { getCategories, addCategory, deleteCategory } from "../controllers/categoriesController.js";
+
+import { getCategories, addCategory, deleteCategory, updateCategory } from "../controllers/categoriesController.js";
 
 const router = express.Router();
 
 router.get("/:userId", getCategories);
+
 router.post("/", addCategory);
+
+router.put("/:id", updateCategory);
+
 router.delete("/:id", deleteCategory);
 
 export default router;
--- backend/src/routes/transactionsRouter.js
+++ backend/src/routes/transactionsRouter.js
@@ -1,5 +1,14 @@
 import express from "express";
-import { getTransactionsByMonth , addTransaction, deleteTransaction, getTransactionsByCategory, searchTransactions, getTransactions } from "../controllers/transactionsController.js";
+
+import {
+    getTransactionsByMonth,
+    addTransaction, 
+    deleteTransaction,
+    getTransactionsByCategory, 
+    searchTransactions, 
+    getTransactions,
+    updateTransaction
+} from "../controllers/transactionsController.js";
 
 const router = express.Router();
 
@@ -7,7 +16,11 @@ router.get("/:userId", getTransactions);
 router.get("/month/:userId/:year/:month", getTransactionsByMonth);
 router.get("/category/:userId/:categoryId", getTransactionsByCategory);
 router.get("/search/:userId", searchTransactions);
+
 router.post("/", addTransaction);
+
+router.put("/:id", updateTransaction);
+
 router.delete("/:id", deleteTransaction);
 
 export default router;
