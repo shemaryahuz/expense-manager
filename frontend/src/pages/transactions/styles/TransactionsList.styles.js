d1a0a4d3dd7bda1ac6a7f3076a047091775e7e3c
Shemaryahu Zalmanov <shemaryahuz@gmail.com>
2025-11-16 14:05:08 +0200

    updated list styles

--- frontend/src/pages/transactions/styles/TransactionsList.styles.js
+++ frontend/src/pages/transactions/styles/TransactionsList.styles.js
@@ -4,6 +4,8 @@ export const transactionsListStyles = {
         minWidth: 650,
         bgcolor: "background.paper",
         mb: 4,
+        borderRadius: 1,
+        boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)"
     },
     tableHead: {
         "& .MuiTableCell-root": { fontWeight: "bold" }
