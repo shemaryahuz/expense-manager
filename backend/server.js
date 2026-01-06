bb29690b9d3b4f8368a7d3ae5a8f69cc87bb0d2a
Shemaryahu Zalmanov <shemaryahuz@gmail.com>
2026-01-06 10:27:47 +0200

    added env variables for client url and server port

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,12 @@ import usersRouter from "./src/routes/usersRouter.js";
 import transactionsRouter from "./src/routes/transactionsRouter.js";
 import categoriesRouter from "./src/routes/categoriesRouter.js";
 
+const CLIENT_URL = env.CLIENT_URL || "http://localhost:5173";
+const PORT = env.PORT || 3000;
+
 const app = express();
 
-app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
+app.use(cors({ credentials: true, origin: CLIENT_URL }));
 app.use(cookieParser());
 app.use(express.json());
 
@@ -19,6 +22,6 @@ app.use("/api/transactions", transactionsRouter);
 app.use("/api/categories", categoriesRouter);
 app.use("/", (req, res) => res.sendStatus(404));
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
